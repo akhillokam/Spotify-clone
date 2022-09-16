bbcd06e129706095fbe9eeeaece7746637b4ba25
akhillokam <shammi.akhil97@gmail.com>
2022-09-15 21:50:44 -0500

    Spotify clone initial commit

---
+++ src/spotify.js
@@ -0,0 +1,28 @@
+export const authEndpoint = "https://accounts.spotify.com/authorize";
+
+const redirectUri = "http://localhost:3000/";
+const clientId = "d9186ba9ba7942719859a9937a351d7f";
+//cheating crud functionality and using these scopes from their API
+const scopes = [
+  "user-read-currently-playing",
+  "user-read-recently-played",
+  "user-read-playback-state",
+  "user-top-read",
+  "user-modify-playback-state",
+];
+
+export const getTokenFromResponse = () => {
+  return window.location.hash
+    .substring(1)
+    .split("&")
+    .reduce((initial, item) => {
+      let parts = item.split("=");
+      initial[parts[0]] = decodeURIComponent(parts[1]);
+
+      return initial;
+    }, {});
+};
+
+export const loginUrl = `${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join(
+  "%20"
+)}&response_type=token&show_dialog=true`;
